42099ba7fbee0f143b0a30482613b782774d7df1
Ruhaan Reddy <ruhaan3008@gmail.com>
2026-07-02 20:35:29 +0100

    Added helpers functions

--- backend-db.rkt
+++ backend-db.rkt
@@ -4,14 +4,14 @@
 (provide (struct-out match) (struct-out user))
 
 ;;db related
-(provide venues teams users)
+(provide stadiums teams users)
 
 
 ;;These are vector as sets do not garruntee the order
 ;;Index in vector is their id
 
 ;;use list-ref
-(define venues (list "New York" "Dallas" "Toronto" "Mexico City"))
+(define stadiums (list "New York" "Dallas" "Toronto" "Mexico City"))
 
 (define teams (vector "Argentina" "England" "USA" "Scotland" "France" "Netherlands"))
 
@@ -21,3 +21,7 @@
 (struct user (username password is-fan) #:mutable)
 
 (define users (list (user "john" "1234" #t) (user "Kevin" "test" #f)))
+
+
+
+;;(struct listing (match-id location seat-price capacity vacancies) #:mutable)
--- db-interface.rkt
+++ db-interface.rkt
@@ -3,27 +3,52 @@
 (require "backend-db.rkt")
 
 (define get-teams (lambda () teams))
-(define get-venues (lambda () venues))
+(define get-stadiums (lambda () stadiums))
+
+(define get-id-internal (lambda (x lst)
+    (define ret-index -1)
+    (define index 0)
+    (for ([i lst])
+        (cond
+            ((equal? i x) (set! ret-index  index))
+            (#t (set! index (+ index 1)))
+        )
+    )
+
+    ;;this should be the value that returns
+    ret-index
+))
+
+(define get-team-id (lambda (x) (get-id-internal x teams)))
+
+(define get-venue-id (lambda (x) (get-id-internal x stadiums)))
+
 (define get-users (lambda () users))
 (define get-user (lambda (x) (list-ref users x)))
 
 (struct session (user-id is-validated) #:mutable)
 
 (define log-in (lambda (username password)
-                 (define isValid #f)
-                 (define id -1)
-                 (define index 0)
-                 (for ([i (length (get-users))])
+  (define isValid #f)
+  (define id -1)
 
-                   (cond
-                     (
-                      (and (equal? (user-username (get-user i)) username)
-                           (equal? (user-password (get-user i)) password)
-                           (set! isValid (or isValid #t))
-                           (set! id index))
-                      )
-                     (#t (set! index (+ index 1)))
-                     ))
-                 ;; I dont understand racket return semantics 
-                 ;; returns
-                 (session id isValid)))
+  (define index 0)
+
+  (for ([i (length (get-users))])
+
+    (cond
+      ((and (equal? (user-username (get-user i)) username)
+            (equal? (user-password (get-user i)) password)
+              (set! isValid (or isValid #t))
+              (set! id index)))
+      (#t (set! index (+ index 1)))
+      )
+  )
+
+  ;; I dont understand racket return semantics 
+  ;; returns
+  (session id isValid)))
+
+(define valid-session? (lambda (x)
+  (session-is-validated x)
+))
